00935d17fcec8b92f9e65cfd93d81b79a738140f
dio_pwl <2317176298@qq.com>
2026-07-14 09:53:00 +0800

    chore: 清理构建产物和IDE缓存，取消git跟踪

--- opencv/build/intermediates/cxx/Debug/1s2h1u5w/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\cj-la\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\MSDK Sample\\opencv\\.cxx\\Debug\\1s2h1u5w\\arm64-v8a" ^
-  opencv_jni_shared
